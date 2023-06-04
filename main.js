6795985cd1fa266279ec74df828f6f4e4cc69795
Kristi Smith <whippedcreem@gmail.com>
2023-06-04 15:35:34 -0400

    js

---
+++ main.js
@@ -0,0 +1,14 @@
+let searchInput = document.getElementById('search');
+
+document.getElementById('get-character').addEventListener
+('click', function() {
+    
+});
+document.getElementById('get-all-characters')
+addEventListener('click', function() {
+
+});
+
+
+
+
