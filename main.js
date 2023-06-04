30fbd8dfb824cc916888c685e7a82ce3ceab0ff2
Kristi Smith <whippedcreem@gmail.com>
2023-06-04 19:45:10 -0400

    search function

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ addEventListener('click', function() {
 fetchAllCharacters();
 });
 
-searchInput.addEventListener('keyup', function() {
-
+searchInput.addEventListener('keyup', function(e) {
+    let searchString = e.target.value;
+    if (searchString && searchString.length > 2) {
+        fetchSearchedCharacters(searchString);
+    }
 });
 
 function fetchRandomCharacter () {
@@ -57,6 +60,35 @@ function fetchAllCharacters() {
                 });
     })
     .catch(error => console.log('Error: ', error));
+}
+//new code 
+function fetchSearchedCharacters(searchString) {
+    fetch('https://swapi.dev/api/people/')
+        .then(response => response.json())
+        .then(data => {
+            let characters = data.results;
+            let filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(searchString.toLowerCase()));
+            let charactersList = document.getElementById('characters');
+            charactersList.innerHTML = ""; // clear the list before appending new characters
+            filteredCharacters.forEach(character => {
+                let li = document.createElement('li');
+                li.textContent = character.name;
+                li.addEventListener('click', function() {
+                    document.getElementById('character').innerHTML = `
+                        <h2>${character.name}</h2>
+                        <p>Height: ${character.height}cm</p>
+                        <p>Mass: ${character.mass}kg</p>
+                        <p>Hair Color: ${character.hair_color}</p>
+                        <p>Skin Color: ${character.skin_color}</p>
+                        <p>Eye Color: ${character.eye_color}</p>
+                        <p>Birth Year: ${character.birth_year}</p>
+                        <p>Gender: ${character.gender}</p>
+                    `;
+                });
+                charactersList.appendChild(li);
+            });
+        })
+        .catch(error => console.log('Error:', error));
 }
 
 
